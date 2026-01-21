81cba95736aa3c67f8cf599625eb643a4124eb08
sonmhhe172086 <sonmhhe172086@fpt.edu.vn>
2026-01-21 22:12:13 +0700

    update

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ require("dotenv").config();
 
 const corsOptions = {
   origin: [
-    process.env.FRONTEND_URL || "http://localhost:3000",
+    process.env.FRONTEND_URL || "https://lck-viet-version1.onrender.com/",
     "http://localhost:3000",
     "http://127.0.0.1:3000"
   ],
